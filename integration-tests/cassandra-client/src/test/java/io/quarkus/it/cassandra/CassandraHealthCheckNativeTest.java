2ff8f07098a9ddb1359f71dfcaa46755ddc61690
tomekl007 <tomasz.lelek@datastax.com>
2020-03-23 16:50:05 +0100

    JAVA-2693 adapt comment

--- integration-tests/cassandra-client/src/test/java/io/quarkus/it/cassandra/CassandraHealthCheckNativeTest.java
+++ integration-tests/cassandra-client/src/test/java/io/quarkus/it/cassandra/CassandraHealthCheckNativeTest.java
@@ -1,6 +1,6 @@
 package io.quarkus.it.cassandra;
 
-// todo do as part of native-test-support task(JAVA-2696)
+// todo do as part of native-image-support task(JAVA-2696)
 //@NativeImageTest
 //public class CassandraHealthCheckNativeTest extends CassandraHealthCheckTest
 //{
