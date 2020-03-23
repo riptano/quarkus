6d29dcdd983ffa730990701c3524151c23d959bc
tomekl007 <tomasz.lelek@datastax.com>
2020-03-23 13:20:11 +0100

    fix docker tests

--- extensions/cassandra-client/deployment/src/test/java/io/quarkus/cassandra/CassandraTestBase.java
+++ extensions/cassandra-client/deployment/src/test/java/io/quarkus/cassandra/CassandraTestBase.java
@@ -4,36 +4,29 @@ import java.io.IOException;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
-import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.FixedHostPortGenericContainer;
+import org.testcontainers.containers.wait.CassandraQueryWaitStrategy;
 
 public class CassandraTestBase {
-    private static GenericContainer<?> cassandraContainer;
+    private static FixedHostPortGenericContainer<?> cassandraContainer;
     private static final String CASSANDRA_311 = "cassandra:3.11";
-    protected static final String CASSANDRA_INTERNAL_PORT = "9042";
+    protected static final int CASSANDRA_INTERNAL_PORT = 9042;
 
     @BeforeAll
-    public static void startCassandraDatabase() throws IOException {
+    public static void startCassandraDatabase() throws IOException, InterruptedException {
         // create the container with the internal Cassandra port exposed as a mapped port.
-        cassandraContainer = new GenericContainer<>(CASSANDRA_311)
-                .withExposedPorts(Integer.parseInt(CASSANDRA_INTERNAL_PORT));
-        // add a shutdown hook to remove the container on shutdown
-        Runtime.getRuntime()
-                .addShutdownHook(
-                        new Thread(
-                                () -> {
-                                    try {
-                                        cassandraContainer.stop();
-                                    } catch (Exception e) {
-                                        // do nothing, we may have already shutdown
-                                    }
-                                }));
+        cassandraContainer = new FixedHostPortGenericContainer<>(CASSANDRA_311)
+                .withFixedExposedPort(CASSANDRA_INTERNAL_PORT, CASSANDRA_INTERNAL_PORT);
+        cassandraContainer.setWaitStrategy(new CassandraQueryWaitStrategy());
+
         // start the container
         cassandraContainer.start();
     }
 
     @AfterAll
     public static void stopCassandraDatabase() {
-        // nothing - it will be stopped in the shutdownHook
+        if (cassandraContainer != null && cassandraContainer.isRunning()) {
+            cassandraContainer.stop();
+        }
     }
-
 }
--- integration-tests/cassandra-client/src/test/java/io/quarkus/it/cassandra/CassandraTestResource.java
+++ integration-tests/cassandra-client/src/test/java/io/quarkus/it/cassandra/CassandraTestResource.java
@@ -4,7 +4,9 @@ import java.util.Collections;
 import java.util.Map;
 
 import org.jboss.logging.Logger;
+import org.testcontainers.containers.FixedHostPortGenericContainer;
 import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.CassandraQueryWaitStrategy;
 
 import io.quarkus.test.common.QuarkusTestResourceLifecycleManager;
 
@@ -12,25 +14,15 @@ public class CassandraTestResource implements QuarkusTestResourceLifecycleManage
     private static final Logger LOGGER = Logger.getLogger(CassandraTestResource.class);
     private static GenericContainer<?> cassandraContainer;
     private static final String CASSANDRA_311 = "cassandra:3.11";
-    protected static final String CASSANDRA_INTERNAL_PORT = "9042";
+    protected static final int CASSANDRA_INTERNAL_PORT = 9042;
 
     @Override
     public Map<String, String> start() {
         // create the container with the internal Cassandra port exposed as a mapped port.
-        cassandraContainer = new GenericContainer<>(CASSANDRA_311)
-                .withExposedPorts(Integer.parseInt(CASSANDRA_INTERNAL_PORT));
-        // add a shutdown hook to remove the container on shutdown
-        Runtime.getRuntime()
-                .addShutdownHook(
-                        new Thread(
-                                () -> {
-                                    try {
-                                        cassandraContainer.stop();
-                                    } catch (Exception e) {
-                                        // do nothing, we may have already shutdown
-                                    }
-                                }));
-        // start the container
+        cassandraContainer = new FixedHostPortGenericContainer<>(CASSANDRA_311)
+                .withFixedExposedPort(CASSANDRA_INTERNAL_PORT, CASSANDRA_INTERNAL_PORT);
+        cassandraContainer.setWaitStrategy(new CassandraQueryWaitStrategy());
+
         LOGGER.infof("Starting Cassandra %s on port %s", CASSANDRA_311, CASSANDRA_INTERNAL_PORT);
         cassandraContainer.start();
         return Collections.emptyMap();
@@ -38,6 +30,8 @@ public class CassandraTestResource implements QuarkusTestResourceLifecycleManage
 
     @Override
     public void stop() {
-        // nothing to do, it will be stopped in the shutdown hook
+        if (cassandraContainer != null && cassandraContainer.isRunning()) {
+            cassandraContainer.stop();
+        }
     }
 }
